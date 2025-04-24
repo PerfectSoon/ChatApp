dbf0cd826ab893f4ee5b03ba0525c8c60636ed23
PerfectSoon <totktosdox@gmail.com>
2025-04-24 16:44:10 +0300

    update settings

--- services/chat_service/settings.py
+++ services/chat_service/settings.py
@@ -4,8 +4,8 @@ class Settings(BaseSettings):
     service_name: str = "chat_service"
     service_description: str = "Chat microservice"
     database_url: str = "postgresql+asyncpg://user2:password2@postgres_service2:5432/chat_db"
-    secret_key: str = 'SUPER_SECRET_KEY'
-    algorithm: str = 'HS256'
+    secret_key: str
+    algorithm: str
 
 
     model_config = SettingsConfigDict(
